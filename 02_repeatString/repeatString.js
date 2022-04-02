0d256f9613a9e33500026cc2f1182c8eb03f9e33
Federico Franco <franco.federi@gmail.com>
2022-04-02 19:43:41 +0200

    solved exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+const repeatStringRecursive = function(string, num) {
+    if (num == 0) {
+        return ''
+    }
+    if (num < 0) {
+        return 'ERROR'
+    }
 
+    return string + repeatString(string, num-1)
+};
+
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result += string
+    }
+    return result
 };
 
 // Do not edit below this line
