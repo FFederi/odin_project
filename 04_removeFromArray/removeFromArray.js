761861ccf12edc71429100e4aabc34cd26450aa3
Federico Franco <franco.federi@gmail.com>
2022-04-03 19:30:43 +0200

    solved exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    //let args = array.from(arguments.slice(1))
 
+    for (let elem in toRemove) {
+        const indexToRemove = array.indexOf(toRemove[elem])
+        if (indexToRemove == -1) {
+            continue
+        }
+        array.splice(indexToRemove, 1)
+    }
+    return array
 };
 
 // Do not edit below this line
