ab0ef0eda17a530eb4caccf42a32565b8e3c8e36
Federico Franco <franco.federi@gmail.com>
2022-04-03 22:22:24 +0200

    solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let sum = 0
+    if ((typeof(first) | typeof(second) !=  "number") | 
+        (first < 0) | (second < 0)) {
+        return "ERROR"
+    }
+    if (first > second) {
+        let temp = 0;
+        temp = first;
+        first = second;
+        second = temp;
+    } 
+    for (let i=first; i<second + 1; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
