251ba3e66a188174b4e78a960797ada0405524d5
Federico Franco <franco.federi@gmail.com>
2022-04-05 18:05:01 +0200

    solved exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let S = s.toUpperCase()
+    let punctless = S.replace(/[.,\/#!$%\^&\*\s;:{}=\-_`~()]/g,"")
+    return punctless === punctless.split("").reverse().join("");
 };
 
 // Do not edit below this line
