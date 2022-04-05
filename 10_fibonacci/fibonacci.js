aa83754346e61d416de2198f43ca8e9d92401bf1
Federico Franco <franco.federi@gmail.com>
2022-04-05 18:29:12 +0200

    solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (typeof(x) === 'string') {
+        var x = parseInt(x)
+    }
+    if (x < 0) {
+        return "OOPS"
+    }
+    if (x <= 1) {
+        return x
+    }
+    let fib2 = fibonacci(x-2);
+    return fib2 + fibonacci(x-1)
 };
 
 // Do not edit below this line
