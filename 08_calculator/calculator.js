5540c2996ad45257aa3b758ad07a78264b135cd1
Federico Franco <franco.federi@gmail.com>
2022-04-05 17:51:29 +0200

    solved remaining exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(toSum) {
+  let sum = 0
+	for (num in toSum) {
+    sum += toSum[num]
+  }
+  
+  //let result = toSum.reduce((a, b) => a + b, 0)
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(toMultiply) {
+  return toMultiply.reduce((a, b) => a * b, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0) {
+    return 1
+  }
+  return x * factorial(x - 1)
 };
 
 // Do not edit below this line
