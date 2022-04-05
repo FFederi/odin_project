137fc995c3f888a3802061d5d40ec2135bc7a896
Federico Franco <franco.federi@gmail.com>
2022-04-05 19:18:14 +0200

    solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxAge = 0
+    let result = 0
+    for (person in people) {
+        if (people[person].yearOfDeath === undefined) {
+            people[person].yearOfDeath = new Date().getFullYear();
+        }
+        let age = people[person].yearOfDeath - people[person].yearOfBirth;
+        if (age > maxAge) {
+            maxAge = age;
+            result = person;
+        }
+    }
+    return people[result]
 };
 
 // Do not edit below this line
